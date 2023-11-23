9fc0b0d8cee5c5395fe865050fceba102259dd2a
marcofinucci <info@marcofinucci.com>
2023-11-23 09:05:20 +0100

    add riepilogo

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ const yourKmEl = document.getElementById("your-km");
 const yourAgeEl = document.getElementById("your-age");
 const submitEl = document.getElementById("submit");
 const discountEl = document.getElementById("discount");
+const ageEl = document.getElementById("age");
+const kmEl = document.getElementById("km");
 const ticketPriceEl = document.getElementById("ticket-price");
 
 const pricePerKm = 0.21;
@@ -29,16 +31,32 @@ submitEl.addEventListener('click', function() {
     const yourKm = yourKmEl.value;
     const yourAge = yourAgeEl.value;
 
+    // Print age
+    if(yourAge == 0) {
+        ageEl.innerHTML = '0 anni'; 
+    } else if (yourAge == 1) {
+        ageEl.innerHTML = '1 anno'; 
+    } else {
+        ageEl.innerHTML = yourAge + ' anni'; 
+    }
+
+    // Print km
+    if(yourKm == 0) {
+        kmEl.innerHTML = '0 km';
+    } else {
+        kmEl.innerHTML = yourKm + ' km'; 
+    }
+
     // Calculate ticket price
     ticketPrice = yourKm * pricePerKm;
 
-    // Calculate ticket price final
+    // Calculate ticket price final and print discount
     if (yourAge < childAge) {
         ticketPriceFinal = priceDiscounted(ticketPrice, percentageDiscountChild);
-        discountEl.innerHTML = 'Sconto <strong>' + percentageDiscountChild + '%</strong> under ' + childAge;
+        discountEl.innerHTML = 'Sconto ' + percentageDiscountChild + '% under ' + childAge;
     } else if (yourAge > seniorAge) {
         ticketPriceFinal = priceDiscounted(ticketPrice, percentageDiscountSenior);
-        discountEl.innerHTML = 'Sconto <strong>' + percentageDiscountSenior + '%</strong> over ' + seniorAge;
+        discountEl.innerHTML = 'Sconto ' + percentageDiscountSenior + '% over ' + seniorAge;
     } else {
         ticketPriceFinal = ticketPrice;
         discountEl.innerHTML = 'Biglietto standard';
